cf452a8e16881b97e6378d4ed8898f0b48bdf99e
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-04-23 12:45:57 +0545

    Started making another block animation

---
+++ Pendulum waves - blocks - 2/sketch.js
@@ -0,0 +1,15 @@
+let width = 1360;
+let height = 600;
+let centreX = width / 2;
+let centreY = height / 2;
+
+function setup() {
+    createCanvas(width, height);
+    frameRate(60);
+}
+
+function draw() {
+    background(50);
+}
+
+
